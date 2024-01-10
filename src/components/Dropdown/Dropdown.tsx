7e66503a163035ad715ad63e7876e08e3d35854b
IntarialN <support@intarial.com>
2024-01-10 17:21:55 +0300

    [DEV-51] Обновление стейтинга в компонентах

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -3,7 +3,7 @@ import './Dropdown.less';
 
 export interface TooltipProps {
   children: string | JSX.Element;
-  open: { value: boolean, update: (val: boolean) => void }
+  open: { value: boolean, open: () => void, close: () => void }
   callbacks: { value: JSX.Element | string, callback: { mouseMove?: () => void, click?: () => void, mouseLeave?: () => void } }[];
 }
 
@@ -16,16 +16,16 @@ const Dropdown = ({
   const click = (callback?: () => void) => {
     callback && callback();
 
-    open.update(false);
+    open.close();
   }
 
   return (
     <div
       className="ev-dropdown"
-      onMouseLeave={ () => open.update(false) }
+      onMouseLeave={ () => open.close() }
     >
       <div
-        onClick={ () => open.update(!open) }
+        onClick={ () => open.open() }
         className={
           [
             "ev-dropdown-box",
@@ -34,7 +34,7 @@ const Dropdown = ({
         }
       >
         { children }
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <svg className='ev-dropdown-box--icon' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
           <path d="M14.9324 6.81641H9.74075H5.06572C4.26572 6.81641 3.86572 7.78307 4.43239 8.34975L8.74908 12.6664C9.44075 13.3581 10.5657 13.3581 11.2574 12.6664L12.8991 11.0247L15.5741 8.34975C16.1324 7.78307 15.7324 6.81641 14.9324 6.81641Z" fill="#515356"/>
         </svg>
       </div>
